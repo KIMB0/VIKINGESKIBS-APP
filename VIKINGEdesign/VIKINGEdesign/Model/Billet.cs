389dd4f38405ddf0c6a853a94d8b7cfc5fb0d6c4
Kim Meyer <Kimsen1992@gmail.com>
2015-04-30 09:31:37 +0200

    Det hele
    :dart: :+1:

---
+++ VIKINGEdesign/VIKINGEdesign/Model/Billet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIKINGEdesign.Model
+{
+    class Billet
+    {
+        private int _billetId;
+        private int _kundeId;
+        private int _antalBørn;
+        private int _antalVoksne;
+        private bool _sejltur;
+        private DateTime _dateTime;
+        private int _pris;
+
+        public int Billet_id
+        {
+            get { return _billetId; }
+            set { _billetId = value; }
+        }
+
+        public int Kunde_id
+        {
+            get { return _kundeId; }
+            set { _kundeId = value; }
+        }
+
+        public int AntalBørn
+        {
+            get { return _antalBørn; }
+            set { _antalBørn = value; }
+        }
+
+        public int AntalVoksne
+        {
+            get { return _antalVoksne; }
+            set { _antalVoksne = value; }
+        }
+
+        public bool Sejltur
+        {
+            get { return _sejltur; }
+            set { _sejltur = value; }
+        }
+        
+
+        public DateTime DateTime
+        {
+            get { return _dateTime; }
+            set { _dateTime = value; }
+        }
+
+        public int Pris
+        {
+            get { return _pris; }
+            set { _pris = value; }
+        }
+        
+    }
+   
+}
---
+++ VIKINGEdesign/VIKINGEdesign/Model/Kunde.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIKINGEdesign.Model
+{
+    class Kunde
+    {
+        private int _kundeId;
+        private string _navn;
+        private string _email;
+        private string _telefonNr;
+
+        public int Kunde_id
+        {
+            get { return _kundeId; }
+            set { _kundeId = value; }
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string TelefonNr
+        {
+            get { return _telefonNr; }
+            set { _telefonNr = value; }
+        }
+    }
+}
