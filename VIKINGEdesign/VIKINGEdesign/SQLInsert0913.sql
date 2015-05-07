f2f4e9b7d97cddc2ec3f087843a733c8e587a1da
kagejohn <kagejohn@kagejohn.dk>
2015-05-07 09:51:03 +0200

    SQL INSERT
    kage

---
+++ VIKINGEdesign/VIKINGEdesign/SQLInsert0913.sql
@@ -0,0 +1,18 @@
+/* Insert values into tables*/
+INSERT INTO Kunder VALUES ('email','navn',12345678);
+
+INSERT INTO Billeter VALUES (1,123456789,123456789,2015-05-07,123.45,0);
+
+INSERT INTO VikingeSkibe VALUES ('Bjørnefjord',20);
+INSERT INTO VikingeSkibe VALUES ('Blakman',9);
+INSERT INTO VikingeSkibe VALUES ('Embla',14);
+INSERT INTO VikingeSkibe VALUES ('Gerifuglen',12);
+INSERT INTO VikingeSkibe VALUES ('Helge Ask',30);
+INSERT INTO VikingeSkibe VALUES ('Kraka Fyr',15);
+INSERT INTO VikingeSkibe VALUES ('Skjoldungen',18);
+INSERT INTO VikingeSkibe VALUES ('Krákan',9);
+INSERT INTO VikingeSkibe VALUES ('Oselven',13);
+INSERT INTO VikingeSkibe VALUES ('Sunnfjord',13);
+INSERT INTO VikingeSkibe VALUES ('Trolle',9);
+
+INSERT INTO Booking VALUES (1,1);
