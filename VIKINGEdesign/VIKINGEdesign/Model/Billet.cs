cb544ad0b5618a23bb9540139c1d8d143aed5878
kagejohn <kagejohn@kagejohn.dk>
2015-05-08 09:56:31 +0200

    Revert "jjhdjdj"
    This reverts commit 8af8462b21c27f6bf68e1baf491da6ae277f8670.

---
+++ VIKINGEdesign/VIKINGEdesign/Model/Billet.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIKINGEdesign.Model
+{
+    class Billet
+    {
+        private int _billetId;
+        private int _kundeId;
+        private int _antalBorn;
+        private int _antalVoksne;
+        private bool _sejltur;
+        private DateTime _dateTime;
+        private int _pris;
+
+        public int Billet_id
+        {
+            get { return _billetId; }
+            set { _billetId = value; }
+        }
+
+        public int Kunde_id
+        {
+            get { return _kundeId; }
+            set { _kundeId = value; }
+        }
+
+        public int AntalBorn
+        {
+            get { return _antalBorn; }
+            set { _antalBorn = value; }
+        }
+
+        public int AntalVoksne
+        {
+            get { return _antalVoksne; }
+            set { _antalVoksne = value; }
+        }
+
+        public bool Sejltur
+        {
+            get { return _sejltur; }
+            set { _sejltur = value; }
+        }
+        
+
+        public DateTime DateTime
+        {
+            get { return _dateTime; }
+            set { _dateTime = value; }
+        }
+
+        public int Pris
+        {
+            get { return _pris; }
+            set { _pris = value; }
+        }
+        
+    }
+   
+}
---
+++ VIKINGEdesign/VIKINGEdesign/Model/Vikingeskib.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIKINGEdesign.Model
+{
+    class Vikingeskib
+    {
+        private int _skibsId;
+        private string _navn;
+        private int _pladser;
+
+
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public int Pladser
+        {
+            get { return _pladser; }
+            set { _pladser = value; }
+        }
+
+
+        public int Skibs_id
+        {
+            get { return _skibsId; }
+            set { _skibsId = value; }
+        }
+    }
+}
