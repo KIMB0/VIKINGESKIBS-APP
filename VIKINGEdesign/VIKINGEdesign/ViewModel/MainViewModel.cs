6668fd5b7ab6bc6db05c071856bbab630dc68149
Kim Meyer <Kimsen1992@gmail.com>
2015-05-13 10:45:35 +0200

    MainViewModel
    Kun meget lidt

--- VIKINGEdesign/VIKINGEdesign/ViewModel/MainViewModel.cs
+++ VIKINGEdesign/VIKINGEdesign/ViewModel/MainViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using VIKINGEdesign.Annotations;
 
 namespace VIKINGEdesign.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : INotifyPropertyChanged
     {
+
+
+
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
     }
 }
