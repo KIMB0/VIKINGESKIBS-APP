949786fc9361609aba17833e10eb4c574de1cc22
kagejohn <kagejohn@kagejohn.dk>
2015-05-28 12:16:47 +0200

    update
    kage

--- VIKINGEdesign/VIKINGEdesign/Handler/BilletHandler.cs
+++ VIKINGEdesign/VIKINGEdesign/Handler/BilletHandler.cs
@@ -23,16 +23,38 @@ namespace VIKINGEdesign.Handler
         public void CreateBillet()
         {
             PrisHandler.TotalPris();
-            MainViewModel.VikingeCatalogSingleton.Add(MainViewModel.AntalBorn, MainViewModel.AntalStuderende, MainViewModel.AntalVoksne,DateTimeConverter.DateTimeOffset(MainViewModel.DateTime), MainViewModel.Pris, MainViewModel.Sejltur, MainViewModel.Email, MainViewModel.Navn, MainViewModel.TelefonNr);
-            if (MainViewModel.Navn.Length == 0)
+            //if (MainViewModel.Navn.Length == 0)
+            //{
+            //    MessageDialog dialog = new MessageDialog("");
+            //    dialog.ShowAsync();
+            //}
+            if (MainViewModel.Navn != "" || MainViewModel.Email != "" || MainViewModel.TelefonNr != "" ||
+                (MainViewModel.AntalBorn + MainViewModel.AntalStuderende + MainViewModel.AntalVoksne) >= 1)
             {
-                MessageDialog dialog = new MessageDialog("Navnet er tomt");
-                dialog.ShowAsync();
+                if (DateTimeConverter.DateTimeOffset(MainViewModel.DateTime) < DateTime.Today)
+                {
+                    MessageDialog dialog = new MessageDialog("Datoen på billet skal være idag eller senere.");
+                }
+                if (MainViewModel.Navn.Length <= 2)
+                {
+                    MessageDialog dialog = new MessageDialog("Navn skal udfyldes med mere end 2 bogstaver");
+                }
+                if (MainViewModel.TelefonNr.Contains(@"(^?!*\+45\d{8}$)|(^?!*\d{8}$)"))
+                {
+                    MessageDialog dialog = new MessageDialog("Telefon nummeret er ikke gyldigt");
+                }
+                if (MainViewModel.Email.Contains(@"^?!*(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$"))
+                {
+                    MessageDialog dialog = new MessageDialog("Emailen er ikke gyldig");
+                }
+                else
+                {
+                    MainViewModel.VikingeCatalogSingleton.Add(MainViewModel.AntalBorn, MainViewModel.AntalStuderende, MainViewModel.AntalVoksne, DateTimeConverter.DateTimeOffset(MainViewModel.DateTime), MainViewModel.Pris, MainViewModel.Sejltur, MainViewModel.Email, MainViewModel.Navn, MainViewModel.TelefonNr);
+                }
             }
-            else if (MainViewModel.Navn.Length > 2)
+            else
             {
-                MainViewModel.VikingeCatalogSingleton.Add(MainViewModel.AntalBorn, MainViewModel.AntalStuderende, MainViewModel.AntalVoksne, DateTimeConverter.DateTimeOffset(MainViewModel.DateTime), MainViewModel.Pris, MainViewModel.Sejltur, MainViewModel.Email, MainViewModel.Navn, MainViewModel.TelefonNr);
-
+                MessageDialog dialog = new MessageDialog("Alle felter skal udfyldes.");
             }
         }
     }
