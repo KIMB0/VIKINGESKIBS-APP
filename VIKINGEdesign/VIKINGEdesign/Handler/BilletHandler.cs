cd860216caa02d5897a582d1c08364791ff4238b
kagejohn <kagejohn@kagejohn.dk>
2015-05-29 13:46:18 +0200

    update
    nu virker input handeling

--- VIKINGEdesign/VIKINGEdesign/Handler/BilletHandler.cs
+++ VIKINGEdesign/VIKINGEdesign/Handler/BilletHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Windows.UI.Popups;
 using VIKINGEdesign.Converter;
@@ -22,39 +23,53 @@ namespace VIKINGEdesign.Handler
 
         public void CreateBillet()
         {
+            MessageDialog dialog = new MessageDialog("");
+            Regex regexTelefonNr1 = new Regex(@"^\d{8}$");
+            Regex regexTelefonNr2 = new Regex(@"^\+45\d{8}$");
+            Regex regexEmail = new Regex(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
             PrisHandler.TotalPris();
             //if (MainViewModel.Navn.Length == 0)
             //{
             //    MessageDialog dialog = new MessageDialog("");
             //    dialog.ShowAsync();
             //}
-            if (MainViewModel.Navn != "" || MainViewModel.Email != "" || MainViewModel.TelefonNr != "" ||
+            if (!string.IsNullOrEmpty(MainViewModel.Navn) && !string.IsNullOrEmpty(MainViewModel.Email) && !string.IsNullOrEmpty(MainViewModel.TelefonNr) &&
                 (MainViewModel.AntalBorn + MainViewModel.AntalStuderende + MainViewModel.AntalVoksne) >= 1)
             {
                 if (DateTimeConverter.DateTimeOffset(MainViewModel.DateTime) < DateTime.Today)
                 {
-                    MessageDialog dialog = new MessageDialog("Datoen på billet skal være idag eller senere.");
+                    dialog.Content += "Datoen på billet skal være idag eller senere.\n";
                 }
                 if (MainViewModel.Navn.Length <= 2)
                 {
-                    MessageDialog dialog = new MessageDialog("Navn skal udfyldes med mere end 2 bogstaver");
+                    dialog.Content += "Navn skal udfyldes med mere end 2 bogstaver.\n";
                 }
-                if (MainViewModel.TelefonNr.Contains(@"(^?!*\+45\d{8}$)|(^?!*\d{8}$)"))
+                if (!regexTelefonNr1.IsMatch(MainViewModel.TelefonNr) && !regexTelefonNr2.IsMatch(MainViewModel.TelefonNr))
                 {
-                    MessageDialog dialog = new MessageDialog("Telefon nummeret er ikke gyldigt");
+                    dialog.Content += "Telefon nummeret er ikke gyldigt.\n";
                 }
-                if (MainViewModel.Email.Contains(@"^?!*(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" + @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$"))
+                if (!regexEmail.IsMatch(MainViewModel.Email))
                 {
-                    MessageDialog dialog = new MessageDialog("Emailen er ikke gyldig");
+                    dialog.Content += "Emailen er ikke gyldig.\n";
                 }
-                else
+                if (
+                        DateTimeConverter.DateTimeOffset(MainViewModel.DateTime) >= DateTime.Today &&
+                        MainViewModel.Navn.Length >= 2 &&
+                        (regexTelefonNr1.IsMatch(MainViewModel.TelefonNr) || regexTelefonNr2.IsMatch(MainViewModel.TelefonNr)) &&
+                        regexEmail.IsMatch(MainViewModel.Email)
+                    )
                 {
                     MainViewModel.VikingeCatalogSingleton.Add(MainViewModel.AntalBorn, MainViewModel.AntalStuderende, MainViewModel.AntalVoksne, DateTimeConverter.DateTimeOffset(MainViewModel.DateTime), MainViewModel.Pris, MainViewModel.Sejltur, MainViewModel.Email, MainViewModel.Navn, MainViewModel.TelefonNr);
+                }
+                if (dialog.Content != "")
+                {
+                    dialog.ShowAsync();
                 }
             }
             else
             {
-                MessageDialog dialog = new MessageDialog("Alle felter skal udfyldes.");
+                dialog.Content += "Alle felter skal udfyldes.";
+                dialog.ShowAsync();
             }
         }
     }
